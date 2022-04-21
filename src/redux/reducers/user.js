83a0d295e7142ffda38f162a4bf4acf10e6d26a6
jrmygp <fat.snorlax17@gmail.com>
2022-04-21 16:10:27 +0700

    feat: created login page (#3)
    * feat: created sign-up page
    
    user can now sign up.
    
    * feat: created login feature
    
    user can now login with their existing account.

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,21 @@
+const initial_state = {
+  id: 0,
+  username: "",
+  email: "",
+};
+
+const userReducer = (state = initial_state, action) => {
+  if (action.type === "USER_LOGIN") {
+    return {
+      ...state,
+      username: action.payload.username,
+      email: action.payload.email,
+      id: action.payload.id
+    };
+  } else if (action.type === "USER_LOGOUT") {
+    return initial_state;
+  }
+  return state;
+};
+
+export default userReducer;
