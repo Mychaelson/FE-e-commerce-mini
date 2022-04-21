332ab0813974cd0ed77bdd667c070238a5ffdf5e
jrmygp <fat.snorlax17@gmail.com>
2022-04-21 15:17:53 +0700

    feat: created sign-up page (#1)
    user can now sign up.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,27 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+
+
+const LoginPage = () => {
+    
+    return (
+        <Flex>
+            <Box
+            border="1px solid black"
+            borderRadius="5px"
+            w="450px"
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="center"
+            ml={4}
+            position="absolute"
+            bg="white"
+            >
+                
+            </Box>
+        </Flex>
+    )
+}
+
+export default LoginPage
