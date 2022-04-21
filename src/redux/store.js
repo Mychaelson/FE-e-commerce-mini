802243bb2129063fd5802574019b3d38eb50c7e7
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-04-21 17:12:16 +0700

    feat: added reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 
 import userReducer from "./reducers/user";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cart";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
