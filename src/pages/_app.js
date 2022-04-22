c487034aa21c7f217ff759f2027323c2759b99e9
jrmygp <fat.snorlax17@gmail.com>
2022-04-22 14:40:01 +0700

    feat: created Navbar on top (#8)
    now user can log out using log out button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,10 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import store from "../redux/store";
+import Nav from "../component/nav";
 import { Provider } from "react-redux";
 import AuthProvider from "../component/AuthProvider";
+
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -12,8 +14,10 @@ function MyApp({ Component, pageProps }) {
     <Provider store={store}>
       <ChakraProvider>
         <AuthProvider>
+          <Nav />
           <Component {...pageProps} />
         </AuthProvider>
+
       </ChakraProvider>
     </Provider>
   );
