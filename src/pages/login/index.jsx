619b23e9525ccb54300cf19d47acc0db68df3023
jrmygp <fat.snorlax17@gmail.com>
2022-04-22 13:33:38 +0700

    ui: made ui changes in login page (#5)
    * feat: created sign-up page
    
    user can now sign up.
    
    * feat: created login feature
    
    user can now login with their existing account.
    
    * ui: made ui changes in login page
    
    its prettier now in login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,51 +1,93 @@
-import { Box, Button, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, Text, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useFormik } from "formik"
-import * as yup from "yup"
+import { useFormik } from "formik";
+import * as yup from "yup";
 import { userLogin } from "../../redux/actions/user";
 import Link from "next/link";
-
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const LoginPage = () => {
-    const [passwordVisible, setPasswordVisible] = useState(false)
-    const dispatch = useDispatch()
-    const userSelector = useSelector((state) => state.user)
-    const router = useRouter()
-    const toast = useToast()
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const dispatch = useDispatch();
+  const userSelector = useSelector((state) => state.user);
+  const router = useRouter();
+  const toast = useToast();
 
-    const formik = useFormik({
-        initialValues: {
-            username: "",
-            password: ""
-        },
-        validationSchema: yup.object().shape({
-            username: yup.string().required("This field is required!"),
-            password: yup.string().required("This field is required!")
-        }),
-        validateOnChange: false,
-        onSubmit: (values) => {
-            setTimeout(() => {
-                dispatch(userLogin(values, formik.setSubmitting))
-            }, 1500)
-        }
-    })
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: yup.object().shape({
+      username: yup.string().required("This field is required!"),
+      password: yup.string().required("This field is required!"),
+    }),
+    validateOnChange: false,
+    onSubmit: (values) => {
+      setTimeout(() => {
+        dispatch(userLogin(values, formik.setSubmitting));
+      }, 1500);
+    },
+  });
 
-    const inputHandler = (event) => {
-        const { value, name } = event.target
-        formik.setFieldValue(name, value)
+  const inputHandler = (event) => {
+    const { value, name } = event.target;
+    formik.setFieldValue(name, value);
+  };
+
+  console.log(userSelector);
+
+  useEffect(() => {
+    if (userSelector.id) {
+      router.push("/sign-up");
     }
-
-    console.log(userSelector)
-    
-    useEffect(() => {
-        if (userSelector.id) {
-            router.push("/sign-up")
-        }
-    })
+  });
   return (
+    // <Center>
     <Flex>
+      {/* LEFT SIDE COMPONENTS */}
+      <Box display="flex" justifyContent="center">
+        <Image
+          src={
+            "https://images.unsplash.com/photo-1650477733936-06fa48166e5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          }
+          width="700px"
+          m={1}
+        />
+        <Flex flexDir="column"  position="absolute" color="white" top={60}>
+          <Text fontSize="5xl">New Here?</Text>
+          <Box display="flex" flexDir="column" justifyContent="center" alignItems="center">
+
+          <Text fontSize="2xl">Sign up here so you won't miss out our great products!</Text>
+        <Link href="/sign-up">
+          <Button bgColor="white" w="100px" ml={3} color="black" borderRadius="20px" mt={4}>
+            Sign Up
+          </Button>
+        </Link>
+          </Box>
+        </Flex>
+      </Box>
+
+      {/* RIGHT SIDE COMPONENTS */}
+
       <Box
         border="1px solid black"
         borderRadius="5px"
@@ -58,30 +100,59 @@ const LoginPage = () => {
         position="absolute"
         bg="white"
         mt={10}
+        top={40}
+        right={60}
       >
         <Heading mt={3}>Login to Your Account</Heading>
         <Box width="100%" p={5}>
-            {/* USERNAME INPUT */}
+          {/* USERNAME INPUT */}
           <FormControl isInvalid={formik.errors.username}>
             <FormLabel>Username</FormLabel>
-            <Input id="inputUsername" name="username" onChange={inputHandler}/>
+            <Input id="inputUsername" name="username" onChange={inputHandler} />
+
             <FormHelperText>{formik.errors.username}</FormHelperText>
           </FormControl>
           {/* PASSWORD INPUT */}
           <FormControl isInvalid={formik.errors.password}>
             <FormLabel>Password</FormLabel>
-            <Input id="inputPassword" name="password" onChange={inputHandler}/>
+            <InputGroup>
+              <Input
+                id="inputPassword"
+                name="password"
+                onChange={inputHandler}
+                type={passwordVisible ? "text" : "password"}
+              />
+              <InputRightElement
+                children={
+                  <Icon
+                    fontSize="lg"
+                    onClick={() => setPasswordVisible(!passwordVisible)}
+                    as={passwordVisible ? IoMdEyeOff : IoMdEye}
+                    sx={{ _hover: { cursor: "pointer" } }}
+                  />
+                }
+              />
+            </InputGroup>
+
             <FormHelperText>{formik.errors.password}</FormHelperText>
           </FormControl>
         </Box>
         <Flex mb={3}>
-        <Button colorScheme="green" w="100px" onClick={formik.handleSubmit} disabled={formik.isSubmitting}>Login</Button>
-       <Link href="/sign-up">
-        <Button colorScheme="blue" w="100px" ml={3}>Sign Up</Button>
-       </Link>
+          <Button
+            // colorScheme="blackAlpha"
+            w="100px"
+            onClick={formik.handleSubmit}
+            disabled={formik.isSubmitting}
+            bgColor="black"
+            color="white"
+          >
+            Login
+          </Button>
         </Flex>
       </Box>
     </Flex>
+    // </Center>
+
   );
 };
 
