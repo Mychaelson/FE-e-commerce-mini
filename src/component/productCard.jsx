c598af4702ba0eecbf9f03fa5d47e6346029b248
givennp <givennatio2@gmail.com>
2022-04-22 15:23:18 +0700

    feat: put search value into params

--- src/component/productCard.jsx
+++ src/component/productCard.jsx
@@ -1,18 +1,20 @@
-import { Box, Image, Text, Flex, Icon, Button } from "@chakra-ui/react";
+import { Box, Image, Text, Flex, Icon, Button, Link } from "@chakra-ui/react";
 import { RiShoppingCart2Fill } from "react-icons/ri";
 
-const ProductCard = ({productName, image_url, price}) => {
+const ProductCard = ({ productName, image_url, price, productId }) => {
   return (
     <Box padding="10px" width="200px" bg="white" shadow="md" margin="10px">
-      <Image
-        borderRadius="10px"
-        width="inherit"
-        height="200px"
-        objectFit="cover"
-        mb="10px"
-        src={image_url}
-        fallbackSrc="http://placehold.jp/300x400.png"
-      />
+      <Link href={`products/${productId}`}>
+        <Image
+          borderRadius="10px"
+          width="inherit"
+          height="200px"
+          objectFit="cover"
+          mb="10px"
+          src={image_url}
+          fallbackSrc="http://placehold.jp/300x400.png"
+        />
+      </Link>
       <Text fontSize="18px" fontWeight="semibold">
         {productName}
       </Text>
