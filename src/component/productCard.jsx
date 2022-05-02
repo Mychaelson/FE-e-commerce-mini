1031933d6e2ca8849ec63c70cb39229fed689a20
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-05-02 22:10:15 +0700

    Feat: added add to cart function

--- src/component/productCard.jsx
+++ src/component/productCard.jsx
@@ -1,7 +1,45 @@
-import { Box, Image, Text, Flex, Icon, Button, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Text,
+  Flex,
+  Icon,
+  Button,
+  Link,
+  useToast,
+} from "@chakra-ui/react";
 import { RiShoppingCart2Fill } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
+import axiosInstance from "../configs/api";
 
 const ProductCard = ({ productName, image_url, price, productId }) => {
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const userSelector = useSelector((state) => {
+    return state.user;
+  });
+
+  const addToCartBtnHandler = async () => {
+    try {
+      const productAddedToCart = {
+        product_id: productId,
+        user_id: userSelector.id,
+      };
+      await axiosInstance.post(`/cart`, productAddedToCart);
+
+      dispatch(fetchUserCart());
+
+      toast({
+        title: "Item added to cart",
+        duration: 2000,
+        isClosable: true,
+        status: "success",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Box padding="10px" width="200px" bg="white" shadow="md" margin="10px">
       <Link href={`products/${productId}`}>
@@ -20,7 +58,7 @@ const ProductCard = ({ productName, image_url, price, productId }) => {
       </Text>
       <Text my="10px">Rp. {price?.toLocaleString()}</Text>
       <Flex>
-        <Button>
+        <Button onClick={addToCartBtnHandler}>
           <Text>Add to Cart</Text>
           <Icon margin="5px" as={RiShoppingCart2Fill} />
         </Button>
